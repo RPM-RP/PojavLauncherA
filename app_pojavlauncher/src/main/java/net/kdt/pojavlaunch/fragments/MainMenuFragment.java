17700b9720e7c2da4adc781cb8fe506c79207b05
artdeell <ultramaksim@gmail.com>
2024-06-18 08:20:40 +0300

    Remember the username

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/fragments/MainMenuFragment.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/fragments/MainMenuFragment.java
@@ -1,10 +1,11 @@
 package net.kdt.pojavlaunch.fragments;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,14 +13,12 @@ import androidx.fragment.app.Fragment;
 
 import net.kdt.pojavlaunch.PojavProfile;
 import net.kdt.pojavlaunch.R;
-import net.kdt.pojavlaunch.Tools;
 import net.kdt.pojavlaunch.extra.ExtraConstants;
 import net.kdt.pojavlaunch.extra.ExtraCore;
-import net.kdt.pojavlaunch.progresskeeper.ProgressKeeper;
 
 public class MainMenuFragment extends Fragment {
     public static final String TAG = "MainMenuFragment";
-
+    private SharedPreferences mUserPrefs;
 
     public MainMenuFragment(){
         super(R.layout.fragment_launcher);
@@ -27,10 +26,14 @@ public class MainMenuFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        mUserPrefs = view.getContext().getSharedPreferences("user", Context.MODE_PRIVATE);
         Button mPlayButton = view.findViewById(R.id.play_button);
         TextView mUserNameText = view.findViewById(R.id.username_textbar);
+        mUserNameText.setText(mUserPrefs.getString("username", ""));
         mPlayButton.setOnClickListener(v -> {
-            PojavProfile.setCurrentProfile(v.getContext(), mUserNameText.getText().toString());
+            String userName = mUserNameText.getText().toString();
+            mUserPrefs.edit().putString("username", userName).apply();
+            PojavProfile.setCurrentProfile(v.getContext(), userName);
             ExtraCore.setValue(ExtraConstants.LAUNCH_GAME, true);
         });
     }
